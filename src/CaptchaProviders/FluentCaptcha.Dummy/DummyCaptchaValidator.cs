b142760bd36df9dff246f6ee74561f484873b161
pichus <dimatyzuk@gmail.com>
2026-05-10 16:33:38 +0300

    configure Dummy captcha validator to reject expected action parameter, since it's not supported for this type of validator

--- src/CaptchaProviders/FluentCaptcha.Dummy/DummyCaptchaValidator.cs
+++ src/CaptchaProviders/FluentCaptcha.Dummy/DummyCaptchaValidator.cs
@@ -7,8 +7,14 @@ public class DummyCaptchaValidator : ICaptchaValidator
     public async Task<CaptchaValidationResult> ValidateAsync(
         string captchaResponseToken,
         string? remoteIp = null,
+        string? expectedAction = null,
         CancellationToken cancellationToken = default)
     {
+        if (expectedAction is not null)
+        {
+            throw new NotSupportedException("Validating the action is not supported by Dummy captcha validator.");
+        }
+
         return CaptchaValidationResult.Success();
     }
 }
