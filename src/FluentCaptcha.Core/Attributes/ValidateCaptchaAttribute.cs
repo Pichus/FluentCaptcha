307e2de0413f68c5236646e078b6df7d23f18a31
pichus <dimatyzuk@gmail.com>
2026-05-11 18:55:38 +0300

    create xml documentation for ValidateCaptchaAttribute

--- src/FluentCaptcha.Core/Attributes/ValidateCaptchaAttribute.cs
+++ src/FluentCaptcha.Core/Attributes/ValidateCaptchaAttribute.cs
@@ -9,15 +9,50 @@ using Microsoft.Extensions.Options;
 
 namespace FluentCaptcha.Core.Attributes;
 
+/// <summary>
+///     Specifies that the controller or the action method this attribute is applied to requires captcha validation.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class ValidateCaptchaAttribute : Attribute, IFilterFactory
 {
+    /// <summary>
+    ///     Gets or sets the expected action name, so the actual action name specified by the client,
+    ///     where the captcha is rendered, can be compared with it.
+    /// </summary>
     public string? ExpectedAction { get; set; }
 
+    /// <summary>
+    ///     Gets or sets the captcha response token source.
+    /// </summary>
+    /// <remarks>
+    ///     By default, the default captcha response token source, set during fluent captcha configuration, is used.
+    /// </remarks>
     public CaptchaResponseTokenSource? CaptchaResponseTokenSource { get; set; }
 
+    /// <summary>
+    ///     Gets or sets the captcha provider name to be used for validation of endpoints that reach this endpoint.
+    /// </summary>
+    /// <remarks>
+    ///     It is strongly advised to pass predefined constants to this parameter.
+    ///     </br>
+    ///     E.g. <c>YourDesiredCaptchaProviderNameConstants.CaptchaProviderName</c>
+    /// </remarks>
     public string? CaptchaProvider { get; set; }
 
+    /// <summary>
+    ///     Gets or sets the captcha response token request header name.
+    /// </summary>
+    /// <remarks>
+    ///     Only takes effect when
+    ///     <see cref="ValidateCaptchaAttribute.CaptchaResponseTokenSource">
+    ///         <c>CaptchaResponseTokenSource</c>
+    ///     </see>
+    ///     is set to
+    ///     <see cref="CaptchaResponseTokenSource.RequestHeader">
+    ///         <c>CaptchaResponseTokenSource.RequestHeader</c>
+    ///     </see>
+    ///     .
+    /// </remarks>
     public string? CaptchaResponseTokenRequestHeaderName { get; set; }
 
     public bool IsReusable => false;
